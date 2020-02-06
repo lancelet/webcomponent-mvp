3155fb034c5264d422fb7112f4efe049461b1686
Jonathan Merritt <j.s.merritt@gmail.com>
2020-02-06 15:50:16 +1100

    WIP: no idea what I am doing

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,21 @@
+const path = require('path');
+
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts' ]
+    },
     output: {
-        path: __dirname + 'dist',
+        path: path.resolve(__dirname, 'dist'),
         publicPath: '/',
         filename: 'bundle.js'
     },
